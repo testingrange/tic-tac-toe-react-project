05de9f023483dd2a20afb568036b684e8fb7f33c
standev <kvpash@gmail.com>
2023-11-26 15:49:30 -0500

    update Player, Gameboard components and App module

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,33 @@
+import { useState } from 'react'
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+
+  const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+  const handleSelectSquire = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol
+        return updatedBoard
+    })
+
+    onSelectSquare()
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquire(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
 
     const [ playerName, setPlayerName ] = useState(initialName)
     const [ isEditing, setIsEditing ] = useState(false)
@@ -25,7 +25,7 @@ export default function Player({initialName, symbol}) {
     }
 
     return (
-        <li>
+        <li className={isActive && 'active'}>
             <span className="player">
               {playerNameFld}
               <span className="player-symbol">{symbol}</span>
